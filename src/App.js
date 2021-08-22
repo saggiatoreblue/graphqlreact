31045cefa7ca2e7622f3f858f60fce4861f8d2d0
Michael Botelho <saggiatoreblue@gmail.com>
2021-08-22 15:19:30 -0400

    update gitignore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+          <i className="bi bi-diagram-2-fill">Repos </i>
+      </h1>
     </div>
   );
 }
