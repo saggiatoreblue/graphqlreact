45539d5ca38bc18678feb77ac6bcc1394c18f6e3
Michael Botelho <saggiatoreblue@gmail.com>
2021-08-22 15:21:42 -0400

    clean commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './custom.scss';
 
-const accessToken = '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';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
